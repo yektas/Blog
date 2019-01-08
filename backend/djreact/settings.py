9b4795c9b3c4af090a7a5a7a77628e0b47ce4f6d
yektas <ahmetsercanyektas@gmail.com>
2019-01-09 02:05:56 +0300

    backend cors problem resolved

--- backend/djreact/settings.py
+++ backend/djreact/settings.py
@@ -38,12 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
-    'ckeditor',
+    'corsheaders',
     'posts',
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
