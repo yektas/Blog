167ff962131d27a4fde373b8e8f7162df6b91a59
yektas <ahmetsercanyektas@gmail.com>
2019-01-10 07:35:51 +0300

    Code sytax highlighting module added. Needs some work to get it right.

--- frontend/src/components/PostEditor2.js
+++ frontend/src/components/PostEditor2.js
@@ -3,14 +3,33 @@ import ReactQuill, { Quill } from 'react-quill';
 import BlotFormatter from "quill-blot-formatter";
 import { ImageDrop } from "quill-image-drop-module";
 
+function MyModule(quill, options){
+    quill.on('selection-change', function(range, oldRange, source) {
+        if (range) {
+          if (range.length !== 0) {
+            var text = quill.getText(range.index, range.length);
+            console.log('User has highlighted', text);
+          }
+        } else {
+          console.log('Cursor not in the editor');
+        }
+      });
+    var range = quill.getSelection();
+    if (range) {
+        if (range.length === 0) {
+          console.log('User cursor is at index', range.index);
+        } else {
+          var text = quill.getText(range.index, range.length);
+          console.log('User has highlighted: ', text);
+        }
+      } else {
+        console.log('User cursor is not in editor');
+      }
+}
+
 Quill.register('modules/blotFormatter', BlotFormatter);
 Quill.register('modules/imageDrop', ImageDrop);
-
-const styles = {
-    editor: {
-        
-    }
-};
+Quill.register('modules/myModule', MyModule);
 
 const modules = {
     toolbar: [
@@ -18,17 +37,17 @@ const modules = {
         ['bold', 'italic', 'underline','strike', 'blockquote'],
         [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
         ['link', 'image'],
-        ['clean']
+        ['clean'],
+        ['code-block']
     ],
-      blotFormatter: {},
-      imageDrop: true
-  }
+    syntax: true,
+}
 
 const formats = [
     'header',
     'bold', 'italic', 'underline', 'strike', 'blockquote',
     'list', 'bullet', 'indent',
-    'link', 'image'
+    'link', 'image', 'code-block'
   ]
 
 class PostEditor2 extends React.Component {
@@ -41,8 +60,7 @@ class PostEditor2 extends React.Component {
     render() {
         return (
             <ReactQuill 
-                theme="bubble"
-                style={styles.editor}
+                theme="snow"
                 value={this.state.text}
                 modules={modules}
                 formats={formats}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import 'react-quill/dist/quill.snow.css';
 import './index.css';
-import 'react-quill/dist/quill.bubble.css';
 import App from './App';
-
 import * as serviceWorker from './serviceWorker';
+
 
 const rootEl = document.getElementById('root');
 ReactDOM.render(<App/>, rootEl);
